926cd64ebd773d28b20f1235360947eff9fcb60e
64v <leguillaume@gmail.com>
2026-04-10 17:01:33 -0700

    build: rebuild all executables

--- tavern/build_guild.py
+++ tavern/build_guild.py
@@ -63,6 +63,7 @@ def build(target_platform: str, onedir: bool = False):
     cmd = [
         sys.executable, "-m", "PyInstaller",
         "--noconfirm",
+        "--clean",
         # Hidden imports - modules loaded dynamically or conditionally
         "--hidden-import", "scaffold",
         "--hidden-import", "scaffold.meta_wizard",
